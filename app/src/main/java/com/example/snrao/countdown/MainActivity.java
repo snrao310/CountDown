0aac6f3e47c9d13a17d2a210791f33a07320b557
Sameeran N Rao <sameerana.nrao@yahoo.com>
2017-01-30 15:19:59 -0700

    Additional Count up timer.

--- app/src/main/java/com/example/snrao/countdown/MainActivity.java
+++ app/src/main/java/com/example/snrao/countdown/MainActivity.java
@@ -16,7 +16,8 @@ import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
-    static final String DO_DATE = "07/24/2017 00:00:00";
+    static final String START_DATE="07/22/2016 23:59:59";
+    static final String DO_DATE = "07/22/2017 00:00:00";
     static final String PREP_DATE = "07/01/2017 00:00:00";
 
     @Override
@@ -25,12 +26,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final TextView doTimer = (TextView) findViewById(R.id.DoTimer);
         final TextView prepTimer=(TextView) findViewById(R.id.PrepTimer);
+        final TextView elapsedTimer=(TextView) findViewById(R.id.ElapsedTimer);
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
         simpleDateFormat.setTimeZone(new SimpleTimeZone(SimpleTimeZone.UTC_TIME, "UTC"));
-        Date today = null, DoDate = null, PrepDate = null;
+        Date today = null, start=null, DoDate = null, PrepDate = null;
         try {
             String now = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new Date());
             today=simpleDateFormat.parse(now); //simpleDateFormat gets it to UTC timezone.
+            start = simpleDateFormat.parse(START_DATE);
             DoDate = simpleDateFormat.parse(DO_DATE);
             PrepDate=simpleDateFormat.parse(PREP_DATE);
         } catch (Exception ex) {
@@ -38,15 +41,18 @@ public class MainActivity extends AppCompatActivity {
         }
         long DoMillis = DoDate.getTime()-today.getTime();
         long PrepMillis = PrepDate.getTime()-today.getTime();
+        final long TotalTime=DoDate.getTime()-start.getTime();
         CountDownTimer DoTimer = new CountDownTimer(DoMillis, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 doTimer.setText("CAN DO IN:\t\t\t\t\t"+getDiffAsString(millisUntilFinished));
+                elapsedTimer.setText("ELAPSED:\t\t\t\t\t\t"+getDiffAsString(TotalTime-millisUntilFinished));
             }
 
             @Override
             public void onFinish() {
                 doTimer.setText("CAN DO NOW!!");
+                elapsedTimer.setText("AN YEAR! You've done it!");
             }
         };
 
@@ -61,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
                 prepTimer.setText("CAN PREP NOW!!");
             }
         };
+
         DoTimer.start();
         PrepTimer.start();
     }
